8278051fe02498754f63644263edf138d6dd48f6
Prince Vashisth <vashisthprince9@gmail.com>
2023-03-21 08:39:20 +0530

    api fd

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false })); 
 
-app.use('/api/upload',post);
+app.use('api/upload',post);
 
 // listening
 app.listen(port,()=>{
